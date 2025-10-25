08cd0b3a5b7b5ff7758f99a41c2aef5a0e5b7939
Emir Büçkün <emirbuckun@hotmail.com>
2025-10-25 12:25:57 +0300

    add generic repository interface and class

---
+++ App.Repositories/GenericRepository.cs
@@ -0,0 +1,20 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Repositories {
+    public class GenericRepository<T>(AppDbContext context) : IGenericRepository<T> where T : class {
+        private readonly DbSet<T> _dbSet = context.Set<T>();
+
+        public IQueryable<T> GetAll() => _dbSet.AsQueryable().AsNoTracking();
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> predicate) => _dbSet.Where(predicate).AsNoTracking();
+
+        public async ValueTask<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+
+        public async ValueTask AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+        public void UpdateAsync(T entity) => _dbSet.Update(entity);
+
+        public void DeleteAsync(T entity) => _dbSet.Remove(entity);
+    }
+}
