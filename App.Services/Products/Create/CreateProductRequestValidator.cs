a8c332572d0eb409260e2ff056542d7f12194496
Emir Büçkün <emirbuckun@hotmail.com>
2025-11-24 12:07:31 +0300

    update product service

--- App.Services/Products/Create/CreateProductRequestValidator.cs
+++ App.Services/Products/Create/CreateProductRequestValidator.cs
@@ -10,6 +10,9 @@ namespace App.Services.Products.Create {
             RuleFor(x => x.Price)
                 .GreaterThan(0).WithMessage("Product price must be greater than zero.");
 
+            RuleFor(x => x.CategoryId)
+                .GreaterThan(0).WithMessage("Product category ID must be greater than zero.");
+
             RuleFor(x => x.Stock)
                 .InclusiveBetween(0, 100).WithMessage("Product stock must be between 0 and 100.");
         }
